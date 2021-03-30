1bf9c0be9b5603ceba927e74684339542616f321
‘Juliano <julianoperins@gmail.com>
2021-03-30 16:32:07 -0400

    Successfully rendered all the items

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,86 +1,56 @@
+import data from "./data";
+
 const App = () => {
   return (
     <div className="grid-container">
       <header className="row">
         <div>
           <a className="brand" href="index.html">
-            amazona
+            Amazing
           </a>
         </div>
         <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
         </div>
       </header>
       <main>
         <div>
           <div className="row center">
-            <div className="card">
-              <a href="product.html">
-                <img className="medium" src="./images/p1.jpg" alt="product" />
-              </a>
-              <div className="card-body">
+            {data.products.map((product) => (
+              <div className="card">
                 <a href="product.html">
-                  <h2>Nike Slim Shirts</h2>
+                  <img
+                    className="medium"
+                    src={product.image}
+                    alt={product.name}
+                  />
                 </a>
-                <div className="rating">
-                  <span>
-                    {" "}
-                    <i className="fa fa-star"></i>{" "}
-                  </span>
-                  <span>
-                    {" "}
-                    <i className="fa fa-star"></i>{" "}
-                  </span>
-                  <span>
-                    {" "}
-                    <i className="fa fa-star"></i>{" "}
-                  </span>
-                  <span>
-                    {" "}
-                    <i className="fa fa-star"></i>{" "}
-                  </span>
-                  <span>
-                    {" "}
-                    <i className="fa fa-star"></i>{" "}
-                  </span>
+                <div className="card-body">
+                  <a href="product.html">
+                    <h2>{product.name}</h2>
+                  </a>
+                  <div className="rating">
+                    <span>
+                      <i className="fa fa-star"></i>{" "}
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>{" "}
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>{" "}
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>{" "}
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>{" "}
+                    </span>
+                  </div>
+                  <div className="price">${product.price}</div>
                 </div>
-                <div className="price">$120</div>
               </div>
-            </div>
-            <div className="card">
-              <a href="product.html">
-                <img className="medium" src="./images/p1.jpg" alt="product" />
-              </a>
-              <div className="card-body">
-                <a href="product.html">
-                  <h2>Nike Slim Shirts</h2>
-                </a>
-                <div className="rating">
-                  <span>
-                    {" "}
-                    <i className="fa fa-star"></i>{" "}
-                  </span>
-                  <span>
-                    {" "}
-                    <i className="fa fa-star"></i>{" "}
-                  </span>
-                  <span>
-                    {" "}
-                    <i className="fa fa-star"></i>{" "}
-                  </span>
-                  <span>
-                    {" "}
-                    <i className="fa fa-star"></i>{" "}
-                  </span>
-                  <span>
-                    {" "}
-                    <i className="fa fa-star"></i>{" "}
-                  </span>
-                </div>
-                <div className="price">$120</div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </main>
