4c4b3f311cac3f833f0ec48292432382a9be49e5
‘Juliano <julianoperins@gmail.com>
2021-04-02 10:34:28 -0400

    Successfully fetched data from the backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Product from "../components/Product";
+import axios from "axios";
 
 import data from "../data";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/products");
+      console.log(data);
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <div className="row center">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <Product key={product._id} product={product} />
         ))}
       </div>
