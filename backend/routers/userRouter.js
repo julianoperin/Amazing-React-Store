a4f3e44ac37e668e881e0343ac7bf6d6549e3c45
‘Juliano <julianoperins@gmail.com>
2021-04-17 14:24:33 -0400

    Stareted working on the user profile

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -58,4 +58,17 @@ userRouter.post(
   })
 );
 
+//! Get User
+userRouter.get(
+  "/:id",
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send({ message: "User not found!" });
+    }
+  })
+);
+
 export default userRouter;
