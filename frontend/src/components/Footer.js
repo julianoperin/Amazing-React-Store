bcee7d1d4d8cc282dc8ca75e2a520630ca5da3b3
‘Juliano <julianoperins@gmail.com>
2021-05-03 22:03:27 -0400

    worked on the footer style

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -10,106 +10,181 @@ const Footer = () => {
       <div className="container-footer">
         <div className="row-footer">
           <div className="footer-col">
-            <h4>company</h4>
+            <h4>Get to Know Us</h4>
             <ul>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  about us
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Careers
                 </a>
               </li>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  our services
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  blog
                 </a>
               </li>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  privacy policy
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  about amazing
                 </a>
               </li>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  affiliate program
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  sustainability
+                </a>
+              </li>
+              <li>
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  press center
+                </a>
+              </li>
+              <li>
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  investor relations
+                </a>
+              </li>
+              <li>
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  amazing devices
+                </a>
+              </li>
+              <li>
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  amazing tours
                 </a>
               </li>
             </ul>
           </div>
           <div className="footer-col">
-            <h4>get help</h4>
+            <h4>make money with us</h4>
             <ul>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  FAQ
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Sell products on amazing
                 </a>
               </li>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  shipping
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Sell apps on amazing
                 </a>
               </li>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  returns
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Become an Affiliate
                 </a>
               </li>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  order status
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Start a package delivery business
                 </a>
               </li>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  payment options
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Advertise Your Products
+                </a>
+              </li>
+              <li>
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Self-Publish with Us
+                </a>
+              </li>
+              <li>
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Host an amazing Hub
+                </a>
+              </li>
+              <li>
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  See More Make Money with Us
                 </a>
               </li>
             </ul>
           </div>
           <div className="footer-col">
-            <h4>indoor plants</h4>
+            <h4>amazing Payment Products</h4>
             <ul>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  outdoor plants
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  amazing Rewards Visa Signature Cards
                 </a>
               </li>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  referrals
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  amazing.com Store Card
                 </a>
               </li>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  gift card
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  amazing Secured Card
                 </a>
               </li>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  amazing Business Line of Credit
+                </a>
+              </li>
+              <li>
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Credit Card Marketplace
+                </a>
+              </li>
+              <li>
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Reload Your Balance
+                </a>
+              </li>
+              <li>
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
                   careers
                 </a>
               </li>
             </ul>
           </div>
           <div className="footer-col">
-            <h4>indoor plants</h4>
+            <h4>Let Us Help You</h4>
             <ul>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  outdoor plants
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  amazing and COVID-19
                 </a>
               </li>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  referrals
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Your Account
                 </a>
               </li>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  gift card
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Your Orders
                 </a>
               </li>
               <li>
-                <a href="https://advertising.amazon.com/?ref=footer_advtsing_amzn_com">
-                  careers
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Shipping Rates & Policies
+                </a>
+              </li>
+              <li>
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  amazing Prime
+                </a>
+              </li>
+              <li>
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Returns & Replacements
+                </a>
+              </li>
+              <li>
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Manage Your Content and Devices
+                </a>
+              </li>
+              <li>
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  amazing Assistant
+                </a>
+              </li>
+              <li>
+                <a href="https://advertising.amazing.com/?ref=footer_advtsing_amzn_com">
+                  Help
                 </a>
               </li>
             </ul>
